69a53c94fa3b0c01a119d0d45a079006c67f7540
Hemanth Veera <148690629+HemanthGit01@users.noreply.github.com>
2024-12-29 20:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    const h1Element = document.querySelector('h1');
+    h1Element.style.color = 'blue';
+    h1Element.innerHTML = 'Hello, World!';
+});
